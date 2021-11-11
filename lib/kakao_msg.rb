7c7a37476f5fc0d5c92a59846065d6166366739e
이수현 <lsh59727@gmail.com>
2021-11-11 14:48:28 +0900

    bizm 알림톡 보내기

---
+++ lib/kakao_msg.rb
@@ -0,0 +1,84 @@
+require 'net/http'
+require 'net/https'
+require 'json'
+
+class KakaoMsg
+  def set_client(user_id, profile)
+    @user_id = user_id
+    @profile = profile
+  end
+
+  def send(
+    phone:, msg:, tmpl_id:,
+    reserve_dt: "00000000000000",
+    button_name: nil, button_url: nil
+  )
+    uri = URI.parse("https://alimtalk-api.bizmsg.kr/v2/sender/send")
+
+    header = {
+      'Content-type': 'application/json;charset=UTF-8',
+      'userid': @user_id
+    }
+    if button_name
+      data = [
+        {
+          message_type: "AT",
+          phn: phone,
+          profile: @profile,
+          reserveDt: reserve_dt,
+          msg: msg,
+          tmplId: tmpl_id,
+          button1: {
+            name: button_name,
+            type: "WL",
+            url_mobile: button_url
+          }
+        }
+      ]
+    else
+      data = [
+        {
+          message_type: "AT",
+          phn: phone,
+          profile: @profile,
+          reserveDt: reserve_dt,
+          msg: msg,
+          tmplId: tmpl_id
+        }
+      ]
+    end
+    request = Net::HTTP::Post.new(uri.path, header)
+    request.body = data.to_json
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    response = http.request(request)
+
+    return response
+  end
+
+  def cancel(
+    msg_id: 
+  )
+    uri = URI.parse("https://alimtalk-api.bizmsg.kr/v2/sender/cancel_reserved")
+
+    header = {
+      'Content-type': 'application/json;charset=UTF-8',
+      'userid': @user_id
+    }
+    data = {
+      msgid: msg_id,
+      profile: @profile,
+    }
+    request = Net::HTTP::Post.new(uri.path, header)
+    request.body = data.to_json
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    response = http.request(request)
+
+    return response
+  end
+end
