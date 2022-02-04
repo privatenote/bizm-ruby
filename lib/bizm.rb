d42b1eeb57fb101f950407daa1309a678c781aec
Gyumin Sim <gyumin.sim@privatenote.co.kr>
2022-02-04 23:45:57 +0900

    feat: 문자 메시지 대체 발송 기능 추가
    Change-Id: I86f9ecf2e0863c371f443bba5a0fc81026f4a922

--- lib/bizm.rb
+++ lib/bizm.rb
@@ -15,7 +15,9 @@ class BizM
     tmpl_id:,
     reserve_dt: '00000000000000',
     button_name: nil,
-    button_url: nil
+    button_url: nil,
+    msg_sms: nil,
+    sms_sender: nil
   )
     uri = URI.parse('https://alimtalk-api.bizmsg.kr/v2/sender/send')
 
@@ -23,36 +25,32 @@ class BizM
       'Content-type': 'application/json;charset=UTF-8',
       'userid': @user_id
     }
+
+    data = {
+      message_type: 'AT',
+      phn: phone,
+      profile: @profile,
+      reserveDt: reserve_dt,
+      msg: msg,
+      tmplId: tmpl_id
+    }
+
     if button_name
-      data = [
-        {
-          message_type: 'AT',
-          phn: phone,
-          profile: @profile,
-          reserveDt: reserve_dt,
-          msg: msg,
-          tmplId: tmpl_id,
-          button1: {
-            name: button_name,
-            type: 'WL',
-            url_mobile: button_url
-          }
-        }
-      ]
-    else
-      data = [
-        {
-          message_type: 'AT',
-          phn: phone,
-          profile: @profile,
-          reserveDt: reserve_dt,
-          msg: msg,
-          tmplId: tmpl_id
-        }
-      ]
+      data[:button1] = {
+        name: button_name,
+        type: 'WL',
+        url_mobile: button_url
+      }
     end
+
+    if msg_sms && sms_sender
+      data[:smsKind] = 'S'
+      data[:msgSms] = msg_sms
+      data[:smsSender] = sms_sender
+    end
+
     request = Net::HTTP::Post.new(uri.path, header)
-    request.body = data.to_json
+    request.body = [data].to_json
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
